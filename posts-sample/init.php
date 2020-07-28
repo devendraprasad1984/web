083a82e5b44a4ebb70c88449918a91d9cb5bf695
devendra prasad cyber renderer <devendraprasad1984@gmail.com>
2020-07-28 21:06:25 +0530

    updates

--- posts-sample/init.php
+++ posts-sample/init.php
@@ -1,2 +1,19 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'ibdn');
+$server = $_SERVER['REMOTE_ADDR'];
+echo 'you are connected to '.$server;
+
+if ($server == 'localhost' or $server == '127.0.0.1') {
+    define('host', 'localhost');
+    define('user', 'root');
+    define('pwd', '');
+    define('db', 'ibdn');
+} else {
+    define('host', 'localhost:3306');
+    define('user', 'wp_p9ii9');
+    define('pwd', 'rbs1984#');
+    define('db', 'wp_8oy2w');
+}
+
+$conn = new mysqli(host, user, pwd, db);
+
+
