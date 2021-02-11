fe348ee2672c93aa749e2e3a75e28f4b644df240
Devendra Prasad <devendraprasad1984@gmail.com>
2021-02-12 00:28:55 +0530

    updates

--- ReactJsApp/cex-front-end/src/components/common/AppButton.js
+++ ReactJsApp/cex-front-end/src/components/common/AppButton.js
@@ -3,7 +3,7 @@ import '../appstyles.css'
 
 const AppButton = props => {
     const {click, val, color} = props
-    const btnColor=color || 'gray white'
+    const btnColor=color || 'black white'
     return <div onClick={click}>
         <span className={`btn ${btnColor}`}>{val}</span>
     </div>
