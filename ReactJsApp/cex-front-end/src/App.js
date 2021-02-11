6f4e4852c9ec2c2d4e02cb112d8705c5c299b7e1
Devendra Prasad <devendraprasad1984@gmail.com>
2021-02-12 00:22:21 +0530

    updates

---
+++ ReactJsApp/cex-front-end/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import {RoutesComponent} from "./components/routes";
+import {BrowserRouter} from 'react-router-dom'
+
+function App() {
+    return <BrowserRouter>
+        <h1>Core eXpansion</h1>
+        <RoutesComponent/>
+    </BrowserRouter>
+}
+
+export default App;
