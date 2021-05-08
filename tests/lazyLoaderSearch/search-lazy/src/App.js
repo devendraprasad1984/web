4f09d73ae06ad38a64ee88a96304ae69a9552f2d
Devendra Prasad <devendraprasad1984@gmail.com>
2021-05-08 17:50:11 +0530

    update

---
+++ tests/lazyLoaderSearch/search-lazy/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SearchBox from "./components/searchBox";
+
+function App() {
+    return (
+        <div className="App">
+            <SearchBox/>
+        </div>
+    );
+}
+
+export default App;
