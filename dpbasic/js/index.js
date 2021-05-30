6fb6164a86305c05da6dbf9b67663d5f2a872b8f
Devendra Prasad <devendraprasad1984@gmail.com>
2021-05-30 17:32:42 +0530

    update

--- dpbasic/js/index.js
+++ dpbasic/js/index.js
@@ -838,15 +838,15 @@ const runAll = () => {
         let appObject = JSON.parse(iskeyset)
         globalObject.welcomeMsg = appObject.name !== '' ? `<div class="labelx xinfo">Welcome, <span class="xred">${appObject.name || 'XXXX'}</span>, you last came on <span class="time xgray">${appObject.lastloggedon || ''}</span></div>` : `<div class="labelx xred">Welcome, Mate!</div>`
         app()
-        handleX('get', undefined, (data) => {
-            welcomeTag.innerHTML = `<h1 class="ml13"><span class="icons size25">visibility</span> <span class="size25">${data.counter.visits || '0'}</span></h1>`
-            // animate('ml13', 3)
-        })
+        // handleX('get', undefined, (data) => {
+        //     welcomeTag.innerHTML = `<h1 class="ml13"><span class="icons size25">visibility</span> <span class="size25">${data.counter.visits || '0'}</span></h1>`
+        //     // animate('ml13', 3)
+        // })
     }
     window.addEventListener('load', () => {
         initCall()
         let payload = JSON.parse(localise())
-        handleX('post', payload)
+        // handleX('post', payload)
         notifyMe('welcome mate!!')
     });
     window.addEventListener('hashchange', initCall)
